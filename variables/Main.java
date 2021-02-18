d34e97d3a242b24aa79489ced0580ee5f9c2243e
Ariq <shadmanmuhtasim2005@gmail.com>
2021-02-18 19:27:39 +0600

    first commit

---
+++ variables/Main.java
@@ -0,0 +1,17 @@
+// package Java.Variables;
+
+public class Main {
+	public static void main(string[] args) {
+		int number = 10;
+		float number_2 = 50;
+		double number_3 = 100;
+		char character = 's';
+		String multiple_character = "Hello everything";
+
+		System.out.printin("There are " + number + "apples");
+		System.out.printin("It costs " + number_2 + " pounds");
+		System.out.printin("It is " + number_3);
+		System.out.printin("I dont know what to do with a char " + character);
+		System.out.printin("Printing multiple character at once aka string " + multiple_character);
+	}
+}
