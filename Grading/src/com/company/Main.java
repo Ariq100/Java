ee8a7b87649c81df39d577d4c9037c0bf6919f59
Ariq <shadmanmuhtasim2005@gmail.com>
2021-03-18 20:51:56 +0600

    fixed

---
+++ Grading/src/com/company/Main.java
@@ -0,0 +1,40 @@
+package com.company;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Enter gpa: ");
+        Scanner input = new Scanner(System.in);
+
+        boolean breakPoint = true;
+
+        do
+        {
+            try
+            {
+                float gpa = input.nextFloat();
+
+                if (gpa >= 50 && gpa <= 100){
+                    System.out.println("congrats you have been promoted");
+                }
+
+                else if (gpa >= 0 && gpa < 50){
+                    System.out.println("Please give a retake and try again next year");
+                }
+
+                else {
+                    throw new Exception();
+                }
+
+                breakPoint = false;
+            }
+            catch (Exception e)
+            {
+                System.out.println("Wrong input");
+
+            }
+        }
+        while(breakPoint == true);
+    }
+}
