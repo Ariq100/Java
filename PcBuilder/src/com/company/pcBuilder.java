be385cde99d08758cb2498069c5ab467e5e68ef6
Ariq <shadmanmuhtasim2005@gmail.com>
2021-04-08 12:35:07 +0600

    backup

--- PcBuilder/src/com/company/pcBuilder.java
+++ PcBuilder/src/com/company/pcBuilder.java
@@ -1,5 +1,5 @@
 package com.company;
-
+// main e 
 public class pcBuilder {
     private static int id;
     private String cpu;
@@ -15,14 +15,21 @@ public class pcBuilder {
     private String keyboard;
     private String mouse;
 
-    public pcBuilder(int cpu, int motherBoard, int ram, int ramCount, String storage, int storageCount, String gpu, String casing, String psu, String monitor, String keyboard, String mouse) {
+    public pcBuilder(int cpu, int motherBoard, int ramCount, int ram, int storage, int gpu, String casing, String psu, String monitor, String keyboard, String mouse) {
         setCpu(cpuChoise(cpu));
         setMotherBoard(motherBoardChoise(motherBoard));
-        setRam(ramChoise(ram));
+        if (ramCount == 8)
+        {
+            setRam(ramChoise8(ram));
+        }
+        else
+        {
+            setRam(ramChoise16(ram));
+        }
         // setRamCount(ramCount);
         setStorage(storage);
         // setStorageCount(storageCount);
-        setGpu(gpu);
+        setGpu(gpuchoise(gpu));
         setCasing(casing);
         setPsu(psu);
         setMonitor(monitor);
@@ -142,11 +149,11 @@ public class pcBuilder {
         }
         else if(cpu == 2)
         {
-            return "2. AMD Ryzen 3 3300G: $99";
+            return "AMD Ryzen 3 3300G: $99";
         }
         else if(cpu == 3)
         {
-            return "3. AMD Ryzen 3 3100: $99";
+            return "AMD Ryzen 3 3100: $99";
         }
 
         return "false";
@@ -156,21 +163,21 @@ public class pcBuilder {
     {
         if (motherBoard == 1)
         {
-            return "1.ASUS ROG Strix B550-F Gaming (WiFi 6) AMD AM4 (3rd Gen Ryzen) ATX Gaming Motherboard: $209.99";
+            return "ASUS ROG Strix B550-F Gaming (WiFi 6) AMD AM4 (3rd Gen Ryzen) ATX Gaming Motherboard: $209.99";
         }
         else if(motherBoard == 2)
         {
-            return "2. MSI B450 GAMING PLUS MAX AM4 AMD B450 SATA 6Gb/s ATX AMD Motherboard: $109.99";
+            return "MSI B450 GAMING PLUS MAX AM4 AMD B450 SATA 6Gb/s ATX AMD Motherboard: $109.99";
         }
         else if(motherBoard == 3)
         {
-            return "3. GIGABYTE X570 AORUS MASTER AMD Ryzen 3000 PCIe 4.0 SATA 6Gb/s USB 3.2 AMD X570 ATX Motherboard: $369.99";
+            return "GIGABYTE X570 AORUS MASTER AMD Ryzen 3000 PCIe 4.0 SATA 6Gb/s USB 3.2 AMD X570 ATX Motherboard: $369.99";
         }
 
         return "false";
     }
 
-    private String ramChoise(int ram)
+    private String ramChoise16(int ram)
     {
         if (ram == 1)
         {
@@ -178,15 +185,43 @@ public class pcBuilder {
         }
         else if(ram == 2)
         {
-            return "2. HyperX Fury 16GB (2x8) 3200MHz DDR4 CL16 DIMM 1Rx8  RGB XMP Desktop Memory dual Stick: $71.00";
+            return "HyperX Fury 16GB (2x8) 3200MHz DDR4 CL16 DIMM 1Rx8  RGB XMP Desktop Memory dual Stick: $71.00";
         }
         else if(ram == 3)
         {
-            return "3. HyperX Fury 8GB 3200MHz DDR4 CL16 DIMM (Kit of 2)  Black XMP Desktop Memory: $86.99";
+            return "HyperX Fury 8GB 3200MHz DDR4 CL16 DIMM (Kit of 2)  Black XMP Desktop Memory: $86.99";
         }
 
         return "false";
     }
+
+    private String ramChoise8 (int ram)
+    {
+        if (ram == 1)
+        {
+            return "Corsair Vengeance LPX 8GB 3200MHz DDR4 Desktop RAM single stick: $50.00";
+        }
+        return "false";
+    }
+
+    private String gpuchoise(int gpu)
+    {
+        if (gpu == 1)
+        {
+            return "GIGABYTE GeForce GTX 1660 OC 6G Graphics Card, 2 x WINDFORCE Fans, 6GB 192-Bit GDDR5, GV-N1660OC-6GD Video Card: $688.90";
+        }
+        else if(gpu == 2)
+        {
+            return "HyperX Fury 16GB (2x8) 3200MHz DDR4 CL16 DIMM 1Rx8  RGB XMP Desktop Memory dual Stick: $71.00";
+        }
+        else if(gpu == 3)
+        {
+            return "HyperX Fury 8GB 3200MHz DDR4 CL16 DIMM (Kit of 2)  Black XMP Desktop Memory: $86.99";
+        }
+
+        return "false";
+    }
+
 //    private boolean cpucompatibility(String cpu, String motherBoard)
 //    {
 //        if (cpu == cpu.contains("Ryzen 3") || cpu == cpu.contains("Ryzen 5") || cpu == cpu.contains("Ryzen 7")
